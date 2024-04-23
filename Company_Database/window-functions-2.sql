4e90f3ba2c41e8e063f3d30c1a201901723075ee
Debaditya Bhar <bhar.debaditya@gmail.com>
2024-04-23 10:24:36 +0530

    Version 1.0.3 - Completed Window Functions.

--- Company_Database/window-functions-2.sql
+++ Company_Database/window-functions-2.sql
@@ -63,4 +63,77 @@ from (
 	  from product
 	 where product_category = 'Phone') phone; 
      
+-- CUME_DIST(): cumulative distribution. 
+-- Value: 0 < cume_dist <= 1
+-- Formula: Current Row No. (or Row No. with the same value as current row) / Total Number of Rows available in the table.
+
+-- Question: Fetch all the product names which constitutes the first 30% of the expensive products based on price. 
+with prod_dist as (
+	select *,
+	-- CUME_DIST() over(ORDER BY price desc) as cume_dist_value,
+	round(CUME_DIST() over(ORDER BY price desc) * 100, 2) as cume_dist_percent_value
+	from product
+)
+
+select product_name,
+	   price,
+       concat(cast(cume_dist_percent_value as CHAR), '%') as cume_dist_val_percent
+  from prod_dist
+ where cume_dist_percent_value <= 30;
+ 
+-- PERCENT_RANK(): Relative rank of the current row. Value: 0 <  PERCENT_RANK <= 1. 
+-- Formula: (Current Row No. - 1) / (Total No. of Rows - 1)
+
+-- Question: How expensive is the Phone - Galaxy S23 Ultra compared to the other phones available in the product table (store).
+
+select * from product where product_category = 'Phone' order by price desc;
+
+with prod_percent_rank as (
+	select *,
+	PERCENT_RANK() OVER(PARTITION BY product_category ORDER BY price) as per_rank,
+	round(PERCENT_RANK() OVER(PARTITION BY product_category ORDER BY price) * 100, 2) as rank_percentage_val
+	from product
+)
+
+select product_name,
+	   product_category,
+       brand,
+       price,
+       concat(cast(rank_percentage_val as CHAR), '%') as price_rank_percentage
+  from prod_percent_rank
+ where product_name = 'Galaxy S23 Ultra';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
      
